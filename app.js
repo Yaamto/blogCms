1a67e52370418eb1e3a753b70597d6acf6c7eb42
Yaamto <73040102+Yaamto@users.noreply.github.com>
2023-09-18 17:43:27 +0200

    test

--- app.js
+++ app.js
@@ -73,9 +73,6 @@ app.use('/admin', adminRoutes);
 
 
 /* Start The Server */
-setTimeout(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}, 500)
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
